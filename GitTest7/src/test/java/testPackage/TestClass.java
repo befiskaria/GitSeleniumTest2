8c5ddb57c740cb88a962326d862dfc2336f27697
befis <befiskaria@gmail.com>
2022-10-01 16:25:22 -0400

    first commit

---
+++ GitTest7/src/test/java/testPackage/TestClass.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	@Test(priority = 4, enabled = true)
+
+	public void TestBefi() throws Exception {
+		//first commit
+		
+	}
+}
